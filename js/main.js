a3699e6af392d060568506ee4510cd800db5d78a
chromecodes <hameedhussain782@gmail.com>
2023-05-20 04:35:38 +0530

    working on dom

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
-import { dom } from "./dom";
+import { datePick } from "./dom/date.js";
+import {btnMaker} from './dom/btn.js'
+
 
 let a1 = document.querySelector('#datePick')
+let a2 = document.querySelector('#btnMaker')
+
+
+
+
+
 
 a1.addEventListener('click',()=>{
     console.log(1)
-    let a = dom.datePick();
+    let a = datePick();
     let c = document.querySelector('.mid')
     console.log(a)
     console.log(c)
 
     c.appendChild(a)
+})
+a2.addEventListener('click',()=>{
+    let a =btnMaker()
+    let c = document.querySelector('.mid')
+    c.appendChild(a)
+
+
 })
