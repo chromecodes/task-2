1387f36a5435136593cd72ca805b19ce96addfbd
chromecodes <hameedhussain782@gmail.com>
2023-05-21 15:17:37 +0530

    fixing bugs

---
+++ js/dom/Autooption.js
@@ -0,0 +1,42 @@
+export const optionMaker = (i,b) => {
+
+    let crt = i;
+    let sad = b.querySelector(`#id-${crt}`)
+
+
+    let li = document.createElement("li");
+    li.setAttribute("class", "optionCnt");
+    li.setAttribute("data-index", `${i}`);
+
+
+        let c2 = document.createElement("input");
+        c2.setAttribute("type", "text");
+        c2.value = `option ${i}`
+
+        c2.addEventListener('input',(e)=>{
+            sad.textContent = e.target.value
+        })
+
+    li.appendChild(c2);
+
+
+        let c3 = document.createElement("input");
+        c3.setAttribute("type", "text");
+        c3.value = `option ${i}`
+
+    li.appendChild(c3);
+
+
+        let c4 = document.createElement("button");
+        c4.setAttribute("type", "button");
+        c4.textContent = 'X'
+
+        c4.addEventListener('click',()=>{
+            li.remove()
+            sad.remove()
+        })
+
+    li.appendChild(c4);
+
+    return li
+}
